73277ccdc315cbe41947aa4e91d0d73600c4411b
SiraphopSangmee <88707085+erksirr@users.noreply.github.com>
2023-12-04 15:56:23 +0700

    Update Source.cpp

--- week2/Source.cpp
+++ week2/Source.cpp
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int x, i;
+	scanf_s("%d", &x);
+	for(i = 2; i <= x;)
+	{
+		if (x % i == 0) {
+			printf(" %d ", i);
+			x = x / i;
+			if (x!=1) {
+				printf("x");
+			}
+		}
+		else if(x%i!=0){
+			i++;
+		}
 
+	}
+	return 0;
+}
